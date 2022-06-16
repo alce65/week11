ce44801628958d43c4fab2d1f586018c4df6369f
Alejandro Cerezo <alce65@hotmail.es>
2022-06-16 21:05:12 +0200

    Initial todo list

---
+++ src/components/todo/list/list.tsx
@@ -0,0 +1,12 @@
+import { Add } from '../add/add';
+import { Task } from '../task/task';
+
+export function List() {
+    return (
+        <>
+            <p>List</p>
+            <Add></Add>
+            <Task></Task>
+        </>
+    );
+}
